2dbc5b066716ecb297fead4eac8338d322ef67af
bjorn <tie.372@gmail.com>
2020-01-22 19:00:51 -0800

    Finger devices/axes; OpenVR finger poses;

--- src/api/l_headset.c
+++ src/api/l_headset.c
@@ -33,6 +33,16 @@ const char* Devices[] = {
   [DEVICE_HEAD] = "head",
   [DEVICE_HAND_LEFT] = "hand/left",
   [DEVICE_HAND_RIGHT] = "hand/right",
+  [DEVICE_HAND_LEFT_FINGER_THUMB] = "hand/left/finger/thumb",
+  [DEVICE_HAND_LEFT_FINGER_INDEX] = "hand/left/finger/index",
+  [DEVICE_HAND_LEFT_FINGER_MIDDLE] = "hand/left/finger/middle",
+  [DEVICE_HAND_LEFT_FINGER_RING] = "hand/left/finger/ring",
+  [DEVICE_HAND_LEFT_FINGER_PINKY] = "hand/left/finger/pinky",
+  [DEVICE_HAND_RIGHT_FINGER_THUMB] = "hand/right/finger/thumb",
+  [DEVICE_HAND_RIGHT_FINGER_INDEX] = "hand/right/finger/index",
+  [DEVICE_HAND_RIGHT_FINGER_MIDDLE] = "hand/right/finger/middle",
+  [DEVICE_HAND_RIGHT_FINGER_RING] = "hand/right/finger/ring",
+  [DEVICE_HAND_RIGHT_FINGER_PINKY] = "hand/right/finger/pinky",
   NULL
 };
 
@@ -55,6 +65,8 @@ const char* DeviceAxes[] = {
   [AXIS_THUMBSTICK] = "thumbstick",
   [AXIS_TOUCHPAD] = "touchpad",
   [AXIS_GRIP] = "grip",
+  [AXIS_CURL] = "curl",
+  [AXIS_SPLAY] = "splay",
   NULL
 };
 
